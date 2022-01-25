536d54ac1e918ea1167b313eaa6cd2aa9eb181b5
Deborah Campos <631care@gmail.com>
2022-01-25 00:20:04 -0500

    Updated background image and buttons

--- src/components/AllTransactions.js
+++ src/components/AllTransactions.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios"
 import { Link } from "react-router-dom"
 import { currencyFormatter } from "./Utility";
+
 const API = process.env.REACT_APP_API_URL
 
 export default function AllTransactions({getTotal}) {
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar({total}) {
     return (
         <nav className="Nav-container">
           <div className="Nav-items">
-            <div><h1>Budget App </h1></div>
+            <div><h2>Budget App </h2></div>
             <div><h3 style={total > 1000 ? {color: "green"}: total < 0? {color: "red"}: {color: "white"}}>Account Total: {currencyFormatter.format(total)}</h3></div>
             <div><button><Link to="/">Transaction Summary</Link></button></div>
             <div className="nav"><button><Link to="/new">New Transaction</Link></button></div>
